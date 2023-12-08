d54150b99ab2349a5449ac77110190b4a8c653bf
anisbastide <anis.bastide@odigo.com>
2023-12-08 12:25:10 +0100

    value between 0 and 255

--- api/gpio.py
+++ api/gpio.py
@@ -2,29 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 # Configuration des broches GPIO
-pin_potentiometer = 4  # Remplacez ceci par le numéro de broche de votre potentiomètre
+pin_potentiometer = 17  # Remplacez ceci par le numéro de broche de votre potentiomètre
 
 # Configuration de la bibliothèque RPi.GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_potentiometer, GPIO.IN)
 
-# Configuration du fichier journal
-log_file_path = "potentiometer_log.txt"
-
-# Fonction pour lire la valeur du potentiomètre
+# Fonction pour lire la valeur du potentiomètre (0-255)
 def read_potentiometer_value():
-    return GPIO.input(pin_potentiometer)
-
-# Fonction pour enregistrer la valeur dans le fichier journal
-def log_value(value):
-    with open(log_file_path, "a") as log_file:
-        log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: {value}\n")
+    return round((GPIO.input(pin_potentiometer) * 255) / 1)  # Conversion de 0/1 à 0/255
 
 # Boucle principale
 try:
     while True:
         pot_value = read_potentiometer_value()
-        print(pot_value)
+        print(f"Valeur du potentiomètre : {pot_value}")
         time.sleep(1)  # Vous pouvez ajuster la fréquence de lecture ici
 
 except KeyboardInterrupt:
